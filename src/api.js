aaf43f9498c36a2ea1573beefba3d0faec438ece
Jonnvoo <Jonathanvu2065@gmail.com>
2023-04-04 15:39:40 -0700

    made the communication with the parent and child

--- src/api.js
+++ src/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-const searchImages = async() =>{
+const searchImages = async(term) =>{
     const response = await axios.get('https://api.unsplash.com/search/photos',{
         headers:{
             Authorization: 'Client-ID JlZJt1y-HoihHRSpbEsvwn9JE2zYaJ2Of2hXzhfXWPw'
 
         },
         params:{
-            query:'dogs'
+            query:term,
         }
     });
-    console.log(response);
-    return response;
+    
+    return response.data.results;
 };
 
 export default searchImages;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+function SearchBar({onSubmit}){
+    const handleClick = () => {
+
+    };
+    return <div>
+        <input/>
+        <button onClick={handleClick}>Click me</button>
+    </div>;
+}
+
+export default SearchBar;
